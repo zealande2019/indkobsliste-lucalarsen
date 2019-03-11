18382682f1fff95e88174f058fd26447a3ec4dc7
lucalarsen <lucalarsen@hotmail.com>
2019-03-11 14:41:19 +0100

    har lavet op til opgave 7

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,17 @@
 //Her skal du skrive din typescriptkode
+document.getElementById('submitbtn').addEventListener("click",addVare);
+let elementInput = document.getElementById("nyvare");
+
+function addVare() {
+    let inputText = (<HTMLInputElement>elementInput).value;
+
+    let listeusund = document.getElementById("listeUsund");
+
+    let nyLi = document.createElement("li");
+    let nyText = document.createTextNode(inputText);
+
+    nyLi.appendChild(nyText);
+    nyLi.setAttribute("class", "usundt");
+
+    listeusund.appendChild(nyLi);
+}
